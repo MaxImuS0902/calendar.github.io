c8ba43ad9c55ff7ff5284870273987d12afe195b
dfernandes_0902 <125720547+MaxImuS0902@users.noreply.github.com>
2023-02-18 22:14:49 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,56 @@
+function calculateDates() {
+  // get input values
+  const startDateInput = document.getElementById('start-date');
+  const breakWeeksInput = document.getElementById('break-weeks');
+  const contentWeeksInput = document.getElementById('content-weeks');
+  
+  const startDate = startDateInput.value;
+  const breakWeeks = parseInt(breakWeeksInput.value, 10);
+  const contentWeeks = parseInt(contentWeeksInput.value, 10);
+
+ // calculate start and end dates for each week
+  const totalWeeks = breakWeeks + contentWeeks;
+  
+  const weeks = [];
+  let currentDate = new Date(startDate);
+  
+  for (let i = 1; i <= totalWeeks; i++) {
+    const weekStartDate = new Date(currentDate);
+    const weekEndDate = new Date(currentDate.getTime() + (7 * 24 * 60 * 60 * 1000));
+    weeks.push({ start: weekStartDate, end: weekEndDate });
+    
+    currentDate = new Date(weekEndDate.getTime() );
+  }
+ 	
+  // display result
+  const resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = '';
+  
+  const resultTable = document.createElement('table');
+  const resultTableHeaderRow = document.createElement('tr');
+  resultTableHeaderRow.innerHTML = '<th>Week</th><th>Start Date</th><th>End Date</th>';
+  resultTable.appendChild(resultTableHeaderRow);
+  
+  for (let i = 0; i < weeks.length; i++) {
+    const weekNumber = i + 0;
+    const weekStartDate = weeks[i].start;
+    const weekEndDate = weeks[i].end;
+    
+    const resultTableRow = document.createElement('tr');
+    resultTableRow.innerHTML = `<td>${weekNumber}</td><td>${weekStartDate.toLocaleDateString()}</td><td>${weekEndDate.toLocaleDateString()}</td>`;
+    
+    if (hasNationalHoliday(weekStartDate, weekEndDate)) {
+      resultTableRow.classList.add('national-holiday');
+    }
+    
+    resultTable.appendChild(resultTableRow);
+  }
+  
+  resultDiv.appendChild(resultTable);
+  resultDiv.style.display = 'block';
+}
+
+function hasNationalHoliday(startDate, endDate) {
+  // replace this with your own logic to check for national holidays
+  return false;
+}
